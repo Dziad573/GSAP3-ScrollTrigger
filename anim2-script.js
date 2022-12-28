f1879cbcc6e7c8aca073dd4b3c863d8b4381f028
Krystian Smagacz <97789391+Dziad573@users.noreply.github.com>
2022-12-28 23:24:52 +0100

    Update anim2-script.js

--- anim2-script.js
+++ anim2-script.js
@@ -4,14 +4,12 @@ const sections = document.querySelectorAll('.a1, .a3');
 sections.forEach(a =>{
     gsap.fromTo(a.children, {x: "-=100", opacity: 0}, {x: 0, opacity: 1, stagger: .2, duration: 1, ease: "easeInOut", scrollTrigger: {
         trigger: a,
-        start: "top 30%",
-        markers: true
+        start: "top 30%"
     }});
 });
 const a2 = document.querySelector('.a2');
 
 gsap.fromTo(a2.children, {y: +100, opacity: 0}, {y: 0, opacity: 1, stagger: .2, duration: 1, ease: "easeInOut", scrollTrigger: {
     trigger: ".a2",
-    start: "top 30%",
-    markers: true
+    start: "top 30%"
 }});
